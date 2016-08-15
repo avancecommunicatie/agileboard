9cbf4066fdb0e14d434ef6c6e5ef94f4ff9ac451
Rob Meijerink <rob.meijerink@gmail.com>
2016-08-15 14:24:32 +0200

    Kalender icoon toegevoegd, zodat urenind niet zelfde icoon heeft

--- resources/views/partials/task_item.blade.php
+++ resources/views/partials/task_item.blade.php
@@ -13,7 +13,7 @@
 		<div class="agile-detail">
 			<div class="row">
 				<div class="col-xl-3 col-lg-6 col-md-6 col-sm-6 col-xs-6">
-					<i class="fa fa-clock-o"></i> {{ date('d-m-Y', $ticket->date_submitted) }}
+					<i class="fa fa-calendar-o"></i> {{ date('d-m-Y', $ticket->date_submitted) }}
 				</div>
 				<div class="col-xl-2 col-lg-3 col-md-3 col-sm-3 col-xs-3" title="Reacties">
 					<i class="fa fa-comment-o"></i> {{ $ticket->bugnote->count() }}
